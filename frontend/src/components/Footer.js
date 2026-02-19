b0601f5419279daefb7475d737ffc9d4631d9364
b <ben.ulaj-1@ou.edu>
2026-02-19 03:02:36 -0600

    Added Footer

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer style={{ 
+        textAlign: "center", 
+        padding: "10px",
+        position: "fixed",
+        bottom: 0,
+       }}>
+      2026© TaskKernel
+    </footer>
+  );
+}
+
+export default Footer;
