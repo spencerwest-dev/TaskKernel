d77f8002de2b87259391a7f2257c376d935d3139
yn3th <162614801+yn3th@users.noreply.github.com>
2026-05-04 01:19:02 -0500

    updates symbol so CI/CD passes

--- backend/src/main/java/com/taskkernel/config/SecurityConfig.java
+++ backend/src/main/java/com/taskkernel/config/SecurityConfig.java
@@ -14,7 +14,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.server.resource.authentication.BearerTokenAuthenticationFilter;
+import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.filter.OncePerRequestFilter;
